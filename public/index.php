08c6844ec6adb5a856fe5a44e4c55029c34bd6fe
Olja <olja.ivkovic97@gmail.com>
2018-05-25 02:32:45 +0200

    start index.php with navigations

---
+++ public/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>STRIPARNICA</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" media="screen" href="utils.css" />
+</head>
+<body>
+  <header>
+    <nav class='log_reg'>
+      <ul>
+        <li><a href='#'>Admin panel</a></li>
+        <li><a href='#'>Account</a></li>
+        <li><a href='#'>Logout</a></li>
+      </ul>
+    </nav>
+    <nav class='navigation'>
+      <ul>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#'>Browse</a></li>
+        <li><a href='#'>My List</a></li>
+        <li><a href='#'>About</a></li>
+        <li><a href='#'>Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+</body>
+</html>
