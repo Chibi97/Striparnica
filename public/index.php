f2371663fc68d52bd2d6522c3e9d42ceba1fdeb7
Olja <olja.ivkovic97@gmail.com>
2018-05-25 04:49:13 +0200

    add navigation and it's animation

--- public/index.php
+++ public/index.php
@@ -5,18 +5,19 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>STRIPARNICA</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" media="screen" href="utils.css" />
+  <link rel="stylesheet" type="text/css" media="screen" href="css/utils.css" />
+  <link rel="stylesheet" type="text/css" media="screen" href="css/home.css" />
 </head>
 <body>
-  <header>
-    <nav class='log_reg'>
+  <header class='flex_col flex_end just_center'>
+    <nav class='log_reg flex_row nav'>
       <ul>
         <li><a href='#'>Admin panel</a></li>
         <li><a href='#'>Account</a></li>
         <li><a href='#'>Logout</a></li>
       </ul>
     </nav>
-    <nav class='navigation'>
+    <nav class='navigation nav'>
       <ul>
         <li><a href='#'>Home</a></li>
         <li><a href='#'>Browse</a></li>
