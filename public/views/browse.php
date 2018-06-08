2775d0fb1cacd7f2b3dbada1fbdfef188b0b2741
Stefan Kupresak <stefan_vg@hotmail.com>
2018-06-08 15:32:44 +0200

    Add submenu in the html

--- public/views/browse.php
+++ public/views/browse.php
@@ -1,5 +1,22 @@
 <div class='browse-container'>
   <div class='filters'>
-    <h1>Filter Results By</h1>
+    <h1 class='text-center'>Filters</h1>
+    <?php $filters = selectMultipleRows($conn, "SELECT name FROM filters"); ?> 
+    <ul>
+      <?php foreach($filters as $filter): ?>
+        <li>
+          <div class='title'>
+            <span><?= $filter->name ?></span> 
+            <i class="fas fa-caret-up"></i>
+          </div>
+          <ul class='sub-items'>
+            <li>Test1</li>
+            <li>Test2</li>
+          </ul>
+        </li>
+      <?php endforeach ?>
+    </ul>
+  </div>
+  <div class='comics'>
   </div>
 </div>
