ea800659ce67283a88b8786cee31d3c8aa6c7a63
Olja <olja.ivkovic97@gmail.com>
2018-06-11 15:16:34 +0200

    start admin panel

--- admin/panel.php
+++ admin/panel.php
@@ -1,2 +1,35 @@
-<?php
-echo "Hello admin Olja";
+
+<form action='#' method='POST' enctype='multipart/form-data' id='dodajStrip'>
+  <h1>Add a new comic</h1>
+  <div class='input-group'>
+    <label>Name of the comic</label>
+    <input type='text' name='comicName' />
+  </div>
+
+  <div class='input-group'>
+    <label>Description</label>
+    <textarea></textarea>
+  </div>
+
+  <div class='input-group'>
+    <label>Number of issues (volumes)</label>
+    <input type='text' name='issues' />
+  </div>
+
+  <div class='input-group'>
+    <label>Choose subfilters</label>
+    <select>
+      <!-- subfilters -->
+    </select>
+  </div>
+
+  <div class='input-group'>
+    <label>Upload 2 pictures</label>
+    <p>Please upload picture that is 250 x 350</p>
+    <input type='file' name='comic-big-pic'>
+    <p>Please upload picture that is 500 x 300</p>
+    <input type='file' name='comic-small-pic'>
+  </div>
+
+  <button>Insert</button>
+</form>
