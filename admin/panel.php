c222d20d1046dd824c67857f6a936863eca38c4d
Olja <olja.ivkovic97@gmail.com>
2018-06-11 22:05:53 +0200

    change css to admin panel

--- admin/panel.php
+++ admin/panel.php
@@ -5,47 +5,47 @@
   }
   
 ?>
+<div class='flex-row center add-comic'>
+  <form action='index.php?page=panel' method='POST' enctype='multipart/form-data' class=''>
+    <h1>Add a new comic</h1>
+    <div class='input-group'>
+      <label>Name of the comic</label>
+      <input type='text' name='comicName' />
+    </div>
 
+    <div class='input-group'>
+      <label>Description</label>
+      <textarea></textarea>
+    </div>
 
-<form action='index.php?page=panel' method='POST' enctype='multipart/form-data' id='dodajStrip'>
-  <h1>Add a new comic</h1>
-  <div class='input-group'>
-    <label>Name of the comic</label>
-    <input type='text' name='comicName' />
-  </div>
+    <div class='input-group'>
+      <label>Number of issues (volumes)</label>
+      <input type='number' name='issues' />
+    </div>
 
-  <div class='input-group'>
-    <label>Description</label>
-    <textarea></textarea>
-  </div>
+    <div class='input-group'>
+      <label>Choose subfilters</label>
+      <select class='multipleSelect' multiple name='tags[]'>
+        <?php 
+          $upit = "SELECT * FROM sub_filters";
+          $filters = selectMultipleRows($conn, $upit);
+          foreach($filters as $filter): ?>
+          <option value="<?= $filter->id ?>"><?= $filter->name ?></option>
+          <?php endforeach ?>
+      </select>
+    </div>
 
-  <div class='input-group'>
-    <label>Number of issues (volumes)</label>
-    <input type='text' name='issues' />
-  </div>
+    <div class='input-group'>
+      <label>Upload 2 pictures</label>
+      <p>Please upload picture that is 250 x 350</p>
+      <input type='file' name='comic-big-pic'>
+      <p>Please upload picture that is 500 x 300</p>
+      <input type='file' name='comic-small-pic'>
+    </div>
 
-  <div class='input-group'>
-    <label>Choose subfilters</label>
-    <select class='multipleSelect' multiple name='tags[]'>
-      <?php 
-        $upit = "SELECT * FROM sub_filters";
-        $filters = selectMultipleRows($conn, $upit);
-        foreach($filters as $filter): ?>
-        <option value="<?= $filter->id ?>"><?= $filter->name ?></option>
-        <?php endforeach ?>
-    </select>
-  </div>
-
-  <div class='input-group'>
-    <label>Upload 2 pictures</label>
-    <p>Please upload picture that is 250 x 350</p>
-    <input type='file' name='comic-big-pic'>
-    <p>Please upload picture that is 500 x 300</p>
-    <input type='file' name='comic-small-pic'>
-  </div>
-
-  <button name='insert'>Insert</button>
-</form>
+    <button name='insert'>Insert</button>
+  </form>
+</div>
 <script>
 window.addEventListener('DOMContentLoaded', () => {
   $(".multipleSelect").fastselect()
--- public/views/head.php
+++ public/views/head.php
@@ -6,6 +6,7 @@
   <link rel="stylesheet" type="text/css" media="screen" href="css/utils.css">
   <link rel="stylesheet" type="text/css" media="screen" href="css/home.css">
   <link rel="stylesheet" type="text/css" media="screen" href="css/browse.css">
+  <link rel="stylesheet" type="text/css" media="screen" href="css/admin.css">
   <link rel="stylesheet" type="text/css" media="screen" href="css/fastselect.css">
   <link rel="icon" href="images/favicon.ico">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp"
