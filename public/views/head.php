7881f6ded4cb5326ac6ba8acf4de81e9d32d0b2c
Olja <olja.ivkovic97@gmail.com>
2018-06-20 14:21:45 +0200

    remove browse css

--- public/views/head.php
+++ public/views/head.php
@@ -4,8 +4,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" media="screen" href="css/utils.css">
-  <link rel="stylesheet" type="text/css" media="screen" href="css/home.css">
-  <link rel="stylesheet" type="text/css" media="screen" href="css/browse.css">
+  <link rel="stylesheet" type="text/css" media="screen" href="css/style.css">
   <link rel="stylesheet" type="text/css" media="screen" href="css/fastselect.css">
   <link rel="icon" href="images/favicon.ico">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp"
