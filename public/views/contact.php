63055bc4536ca1092528f2439c3304a76d59f59e
Olja <olja.ivkovic97@gmail.com>
2018-06-20 14:34:49 +0200

    add html and css for contact form

---
+++ public/views/contact.php
@@ -0,0 +1,18 @@
+<div class='flex-row center add-comic'>
+  <form action='#' method='POST'>
+    <h1>Contact us</h1>
+    <div class='input-group'>
+      <label>Your email address</label>
+      <input type='text' name='comicName' id='comicName' />
+    </div>
+    <span class='form-error'><?= error_for("comicName", "comicErrors"); ?></span>
+
+    <div class='input-group'>
+      <label>Your message...</label>
+      <textarea name='desc' id='desc'></textarea>
+    </div>
+    <span class='form-error'><?= error_for("description", "comicErrors"); ?></span>
+
+    <button id='insertComic' name='contact'>Contact us</button>
+</form>
+</div>
