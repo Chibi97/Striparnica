3aba044d15b2a4551be96743e6a4c18bd9b51c94
Stefan Kupresak <stefan_vg@hotmail.com>
2018-06-22 01:14:11 +0200

    Add panel navigation add chartist

---
+++ admin/stats.php
@@ -0,0 +1,13 @@
+<div class='flex-row center panel-navigation'>
+  <a href='index.php?page=panel'>Comics</a>
+  <a href='index.php?page=stats'>Stats</a>
+</div>
+<div class='flex-row center'>
+
+</div>
+
+<script>
+  window.addEventListener('DOMContentLoaded', () => {
+
+  });
+</script>
