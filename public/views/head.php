244ea90a4faf0a1d5b7b5cf570bb0f3b14c3753e
Olja <olja.ivkovic97@gmail.com>
2018-06-24 03:44:56 +0200

    add meta tags and update documentation

--- public/views/head.php
+++ public/views/head.php
@@ -1,6 +1,9 @@
 <head>
   <title>MyComicsList</title>
   <meta charset="utf-8" />
+  <meta name="keywords" content="browse, comic, login, register, list, read, manga, book, fantasy, horror, action, crime, science, favorites, filters, vote"/>
+  <meta name="description" content="Ever wanted to keep track of the comics that you are interested it? Well, we have a solution for you! We'll give you one list, so you decide what you'll keep track of!"/>
+  <meta name="author" content="mailto:olja.ivkovic.34.16@ict.edu.rs"/>
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
